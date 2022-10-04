a54a259b2ca3b421b7b35dea1041cc7963939e5f
lauridsulso <laurids_ulso@hotmail.com>
2022-10-04 18:08:16 +0200

    contact page skeleton

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,44 @@
 import React from "react";
+import production from "../assets/production.jpg";
 
 const Contact = () => {
   return (
-    <div
-      id="contact"
-      className="h-screen m-0 p-0h-screen flex flex-row justify-center items-center flex flex-row"
-    >
-      <div>tekst here</div>
-      <div>image here</div>
+    <div id="contact" className="h-screen m-0 p-16 h-screen font-oswald">
+      <div className="flex flex-row justify-around">
+        <div className="basis-2/5">
+          <h1 className="text-8xl">
+            Wanna <br></br> collaborate?
+          </h1>
+        </div>
+        <div className="basis-2/5">
+          <p className="text-lg">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore
+            veritatis, animi amet quae adipisci unde obcaecati tempora
+            assumenda, perspiciatis sint possimus sapiente minus ab iste maxime
+            doloribus rerum natus deserunt. <br /> <br />
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore
+            veritatis, animi amet quae adipisci unde obcaecati tempora
+            assumenda, perspiciatis sint possimus sapiente minus ab iste maxime
+            doloribus rerum natus deserunt.
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-row justify-around mt-16">
+        <div className="basis-2/5">
+          <img className="w-full" src={production} alt="" />
+        </div>
+        <div className="basis-2/5 bg-gray-500 px-16 py-6 relative">
+          <p className="text-lg">Lorem ipsum dolor sit amet consectetur</p>
+          <h2 className="text-4xl font-bold text-white">
+            adipisicing elit. Quae accusamus debitis velit commodi non
+            voluptatum
+          </h2>
+          <button className="absolute bottom-10 m-auto w-4/5 hover:bg-transparent bg-black hover:text-black font-semibold text-white py-2 px-4 border hover:border-black border-transparent">
+            GET IN TOUCH
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,26 +1,34 @@
 import React from "react";
 import videoBg from "../assets/video.mp4";
-import scroll_down from "../assets/scroll-bar.png";
+import circle from "../assets/circle.png";
+import arrow from "../assets/arrow.png";
 
 const Main = () => {
   return (
-    <div className="w-full h-screen m-0 p-0">
-      <div className="overlay"></div>
-      <video
-        className="w-full h-full object-cover"
-        src={videoBg}
-        autoPlay
-        loop
-        muted
-      />
-      <div className="absolute w-full h-full top-0 flex flex-col justify-center items-center text-white">
-        <h1>KELOPE PICTURES</h1>
-        <div className="scroll_down">
-          <a href="#contact">
-            <img className="pulsate-fwd" src={scroll_down} alt="" />
-          </a>
+    <div className="w-full h-screen m-0 p-0 font-oswald">
+      <a href="#contact">
+        <div className="h-full w-full opacity-40 bg-black absolute"></div>
+
+        <video
+          className="w-full h-full object-cover"
+          src={videoBg}
+          autoPlay
+          loop
+          muted
+        />
+
+        <div className="absolute w-full h-full top-0 flex flex-col justify-center items-center text-white">
+          <h1 className="text-9xl">KELOPE PICTURES</h1>
+          <div className="flex justify-center w-1/4 mt-16">
+            <img className="relative w-16" src={circle} alt="" />
+            <img
+              className="absolute animate-bounce w-6 mt-6"
+              src={arrow}
+              alt=""
+            />
+          </div>
         </div>
-      </div>
+      </a>
     </div>
   );
 };
